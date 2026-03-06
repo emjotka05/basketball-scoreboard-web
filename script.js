3f9d9830c39e8657f51703a04265014eae7be10e
emjot <emjot@DESKTOP-J3RZY>
2026-03-07 00:04:18 +0100

    Add hover and highlighteffect

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let homeEl = document.getElementById("home-score")
 let guestEl = document.getElementById("guest-score")
+let homeTitleEl = document.getElementById("home")
+let guestTitleEl = document.getElementById("guest")
+
 let homeScore = 0
 let guestScore = 0
 
@@ -9,6 +12,7 @@ function resetScore(){
     guestScore = 0
     homeEl.textContent = homeScore
     guestEl.textContent = guestScore
+    highlight()
 }
 
 resetScore()
@@ -18,34 +22,62 @@ resetScore()
 function homeAddOne(){
     homeScore += 1
     homeEl.textContent = homeScore 
+    highlight()
 }
 
 function guestAddOne(){
     guestScore += 1
     guestEl.textContent = guestScore 
+    highlight()
 }
 
 function homeAddTwo(){
     homeScore += 2
-    homeEl.textContent = homeScore 
+    homeEl.textContent = homeScore
+    highlight() 
 
 }
 
 function guestAddTwo(){
     guestScore += 2
-    guestEl.textContent = guestScore 
+    guestEl.textContent = guestScore
+    highlight() 
    
 }
 
 function homeAddThree(){
     homeScore += 3
-    homeEl.textContent = homeScore 
+    homeEl.textContent = homeScore
+    highlight() 
    
 }
 
 function guestAddThree(){
     guestScore += 3
-    guestEl.textContent = guestScore 
+    guestEl.textContent = guestScore
+    highlight()
    
+}
+
+function highlight(){
+
+    let color = "#EEEEEE"
+    let highlightColor = "#FFFFFF"
+    let glow = "0px 0px 5px #FFFFFF"
+
+    homeTitleEl.style.color = color
+    homeTitleEl.style.textShadow = "none"
+
+    guestTitleEl.style.color = color
+    guestTitleEl.style.textShadow = "none"
+
+    if (homeScore > guestScore){
+        homeTitleEl.style.color = highlightColor
+        homeTitleEl.style.textShadow = glow
+    } else if (guestScore > homeScore){
+        guestTitleEl.style.color = highlightColor
+        guestTitleEl.style.textShadow = glow
+    }
+    
 }
 
